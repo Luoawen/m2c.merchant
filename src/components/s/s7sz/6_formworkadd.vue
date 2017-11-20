df0e1aa045e1f8ba41314c38d19b8317142f2bb4
liuxiaofang <liuxf@lcjh.com>
2017-11-20 12:51:36 +0800

    运费模板新增跳转bug

--- src/components/s/s7sz/6_formworkadd.vue
+++ src/components/s/s7sz/6_formworkadd.vue
@@ -422,14 +422,10 @@
         </div>
         <div class="form-group">
           <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit"
-                    class="btn btn-info btn-lg save"
-                    @click="save()"> 保存
-            </button>
-            <button type="submit"
-                    class="btn btn-default btn-lg"
-                    @click="back()"> 取消
-            </button>
+            <el-button type="primary" @click="save()"> 保存
+            </el-button>
+            <el-button @click="back()"> 取消
+            </el-button>
           </div>
         </div>
         <div class="delectSizeWrap" v-show="delectBg">
@@ -1052,7 +1048,7 @@
             that.postageModelRules = []
             if (result.status === 200) {
               that.show_tip('保存成功')
-              that.$goRoute({path: 'formwork'})
+              that.$router.push({name: 'formwork'})
             } else {
               that.show_tip(result.errorMessage)
             }
